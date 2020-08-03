5c309342ca1c62c3a310d669fc58812833e0f9df
Eria <c121680118@gmail.com>
2020-08-04 03:06:27 +0800

    first commit

---
+++ Section_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Section_1
+//
+//  Created by 阿騰 on 2020/8/4.
+//  Copyright © 2020 阿騰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
